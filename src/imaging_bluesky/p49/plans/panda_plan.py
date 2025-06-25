9a36a588ce1a158d9994aadf172ee97b0d8afa16
Paula <paula.martin-gomez@diamond.ac.uk>
2025-06-25 17:32:45 +0000

    Update Panda plan after test on p49

--- src/imaging_bluesky/p49/plans/panda_plan.py
+++ src/imaging_bluesky/p49/plans/panda_plan.py
@@ -1,11 +1,13 @@
-import math as mt
+from math import ceil
 
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
 from bluesky.utils import MsgGenerator
 from dodal.plan_stubs.data_session import attach_data_session_metadata_decorator
 from ophyd_async.core import (
+    DetectorTrigger,
     StandardFlyer,
+    TriggerInfo,
 )
 from ophyd_async.epics.motor import FlyMotorInfo, Motor
 from ophyd_async.fastcs.panda import (
@@ -17,10 +19,9 @@ from ophyd_async.fastcs.panda import (
 
 # for calculations
 MRES = -0.000125
-PREFIX = "BL49P"
 
 
-def fly_scan_ts(
+def fly_scan(
     start: int,
     stop: int,
     num: int,
@@ -43,29 +44,33 @@ def fly_scan_ts(
             time_for_move=num * duration,
         )
         panda_pcomp_info = PcompInfo(
-            start_postion=mt.ceil(start_pos / (MRES)),
-            pulse_width=mt.ceil(abs((width / 2) / MRES)),
-            rising_edge_step=mt.ceil(abs(width / MRES)),
+            start_postion=ceil(start_pos / abs(MRES)),
+            pulse_width=1,
+            rising_edge_step=ceil(abs(width / MRES)),
             number_of_pulses=num,
             direction=PandaPcompDirection.NEGATIVE
             if width / MRES > 0
             else PandaPcompDirection.POSITIVE,
         )
 
-        #        panda_hdf_info = TriggerInfo(
-        #            number_of_events=num,
-        #            trigger=DetectorTrigger.CONSTANT_GATE,
-        #            livetime=duration,
-        #            deadtime=1e-5,
-        #        )
+        panda_hdf_info = TriggerInfo(
+            number_of_events=num,
+            trigger=DetectorTrigger.CONSTANT_GATE,
+            livetime=duration,
+            deadtime=1e-5,
+        )
 
+        # The order of these prepare calls does not matter, as we are setting the PVs.
         yield from bps.prepare(motor, motor_info)
-        # yield from bps.prepare(panda, panda_hdf_info)
+        yield from bps.prepare(panda, panda_hdf_info)
         yield from bps.prepare(panda_pcomp, panda_pcomp_info, wait=True)
-        # yield from bps.kickoff(panda)
+
+        # Kickoff the motor last to ensure other components are initialized first.
+        # Otherwise, the motor might move before other parts are ready.
+        yield from bps.kickoff(panda)
         yield from bps.kickoff(panda_pcomp, wait=True)
         yield from bps.kickoff(motor, wait=True)
-        # yield from bps.complete_all(motor, panda_pcomp, panda, wait=True)
-        yield from bps.complete_all(motor, panda_pcomp, wait=True)
+
+        yield from bps.complete_all(motor, panda_pcomp, panda, wait=True)
 
     yield from inner_plan()

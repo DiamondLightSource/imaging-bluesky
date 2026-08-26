ee5a15f32a4cc6d5b8d47272c06d53883ccd1398
Srishty <srishty.sajeev@diamond.ac.uk>
2026-08-26 15:09:37 +0100

    comment out p49 plans - incompatible with updated ophyd async

--- src/imaging_bluesky/p49/devices/stages.py
+++ src/imaging_bluesky/p49/devices/stages.py
@@ -1,14 +1,14 @@
-from ophyd_async.core import (
-    StandardReadable,
-)
-from ophyd_async.epics.motor import Motor
+# from ophyd_async.core import (
+#     StandardReadable,
+# )
+# from ophyd_async.epics.motor import Motor
 
 
-class Stages(StandardReadable):
-    """Collection of motors to control the alignment stage"""
+# class Stages(StandardReadable):
+#     """Collection of motors to control the alignment stage"""
 
-    def __init__(self, beamline_prefix: str, name: str = "") -> None:
-        with self.add_children_as_readables():
-            self.x = Motor(beamline_prefix + "-MO-MAP-01:STAGE:X", "x")
-            self.theta = Motor(beamline_prefix + "-MO-MAP-01:STAGE:A", "theta")
-        super().__init__(name=name)
+#     def __init__(self, beamline_prefix: str, name: str = "") -> None:
+#         with self.add_children_as_readables():
+#             self.x = Motor(beamline_prefix + "-MO-MAP-01:STAGE:X", "x")
+#             self.theta = Motor(beamline_prefix + "-MO-MAP-01:STAGE:A", "theta")
+#         super().__init__(name=name)

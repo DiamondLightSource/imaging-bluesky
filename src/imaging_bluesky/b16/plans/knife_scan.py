e231119be426309024d641a0e63d26744bb0fc47
Srishty <srishty.sajeev@diamond.ac.uk>
2026-09-08 17:05:57 +0100

    adding changes from the beamline

--- src/imaging_bluesky/b16/plans/knife_scan.py
+++ src/imaging_bluesky/b16/plans/knife_scan.py
@@ -1,13 +1,14 @@
 """
-A knife scan to determine the correct focus a certian distance from the beam.
+A knife edge scan to determine the correct focus a certian distance from the beam.
 Testing movement first.
 """
 
 # from dodal.devices import Motor
 import bluesky.plan_stubs as bps
+from bluesky.utils import MsgGenerator
 from dodal.common import inject
 from ophyd_async.epics.motor import Motor
 
 
-def move_motor(pos: float, attol1: Motor = inject("attol1")):
-    yield from bps.mv(attol1, pos)
+def move_motor(pos: float, motor: Motor = inject("sim_motor_x")) -> MsgGenerator[None]:
+    yield from bps.mv(motor, pos)
